390e87019cdd1ae76728a78673bf7ca502e48160
MOEUNJI <ejm5004@naver.com>
2022-06-20 19:58:47 +0900

    aa

---
+++ script/script.js
@@ -0,0 +1,11 @@
+// document.addEventListener("mousemove", parallax);
+//         function parallax(e) {
+//             this.querySelectorAll('.hashtag').forEach(hashtag => {
+//                 const speed = hashtag.getAttribute('data-speed');
+
+//                 const x = (window.innerWidth - e.pageX * speed) / 300;
+//                 const y = (window.innerHeight - e.pageY * speed) / 30;
+
+//                 hashtag.style.transform = `translateX(${x}px) translateY(${y}px)`;
+//             })
+//         }
